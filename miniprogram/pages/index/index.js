4eefd074ba287e99203e0516ac2ee694e97e2b1e
KingJA <kingjavip@gmail.com>
2020-01-02 17:17:10 +0800

    2

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,11 +4,8 @@ const db = wx.cloud.database();
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    images: [],
+
   },
 
   insert: function() {
@@ -50,7 +47,7 @@ Page({
     })
   },
 
-  search: function () {
+  search: function() {
     db.collection('user').where({
       name: 'tom'
     }).get().then(res => {
@@ -58,13 +55,117 @@ Page({
     }).catch(error => {
       console.log('search error:' + error);
     })
-  }, delete: function () {
+  },
+  delete: function() {
     db.collection('user').doc('dbff9fc75e05b46107ae77aa1947ae94').remove().then(res => {
-      console.log( res);
+      console.log(res);
     }).catch(error => {
       console.log('update error:' + error);
     })
   },
+  sum: function() {
+    wx.cloud.callFunction({
+      name: 'sum',
+      data: {
+        a: 2,
+        b: 3
+      }
+    }).then(res => {
+      console.log(res);
+    }).catch(error => {
+      console.log(error);
+    });
+  },
+  getOpenId: function() {
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then(res => {
+      console.log(res);
+    }).catch(error => {
+      console.log(error);
+    });
+  },
+  batchDelete: function() {
+    wx.cloud.callFunction({
+      name: 'batchDelete'
+    }).then(res => {
+      console.log(res);
+    }).catch(error => {
+      console.log(error);
+    });
+  },
+  upload: function() {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+
+        // 将图片上传至云存储空间
+        wx.cloud.uploadFile({
+          // 指定上传到的云路径
+          cloudPath: new Date().getTime() + '.png',
+          // 指定要上传的文件的小程序临时文件路径
+          filePath: tempFilePaths[0],
+          // 成功回调
+          success: res => {
+            console.log('上传成功', res)
+            db.collection('image').add({
+              data: {
+                fileID: res.fileID
+              }
+
+            }).then(res => {
+              console.log(res);
+            }).catch(error => {
+              console.error(error);
+            });
+          },
+        })
+      }
+    })
+  },
+  getFile: function() {
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then(res => {
+      db.collection('image').where({
+        _openid: res.result._openid
+      }).get().then(res2 => {
+        console.log(res2);
+
+        this.setData({
+          images: res2.data
+        });
+
+      })
+    }).catch(error => {
+      console.log(error);
+    });
+  },
+  downloadFile: function(event) {
+    console.log('开始下载' + event.target.dataset.fileid);
+    wx.cloud.downloadFile({
+      fileID: event.target.dataset.fileid, //仅为示例，并非真实的资源
+      success(res) {
+        console.log(res.tempFilePath);
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success(res) {
+            wx.showToast({
+              title: '保存成功',
+            })
+            console.log(res);
+          }
+        })
+      },
+      fail(error) {
+        console.log(error);
+      }
+    })
+  }
 
 
 })
